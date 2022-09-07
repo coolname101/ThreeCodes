7bee8424d4ee3717bbdfb2981baa840e69581661
Itumeleng Kesebonye <tux.kesebonye@gmail.com>
2022-09-07 07:33:41 +0200

    Create Mad_Libs.py
    Story-telling.
    Short story to lighten the mood.

---
+++ Mad_Libs.py
@@ -0,0 +1,14 @@
+name = input("Enter your name:")
+best_friend = input("Enter your best friend's name:")
+sibling = input("Enter your sibling's name:")
+favourite_sport = input("Enter your favourite sport:")
+favourite_season = input("Enter your prefered season:")
+
+
+story = ("Three retirees, each with a hearing loss, were playing \
+" + favourite_sport + " one fine " + favourite_season + " day." + sibling + " \
+remarked to " + best_friend + ", 'Windy, isn't it?' 'No', " + best_friend + " replied,\
+'it's Thursday'. "+ name + " chimed in, 'So am I. Let's have a beer.'")
+
+
+print(story)
